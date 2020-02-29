0b13d7e51a4f3c61e832aead6ac3e26f198ac3ef
cyrillecf <cyril.fourcin@gmail.com>
2020-02-29 14:55:33 +0100

    chap8

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -2,7 +2,8 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from '../models/pokemon';
 import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
-  
+
+
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   
