a7860863814fc6befd385f22eca027224971d2f7
cyrillecf <cyril.fourcin@gmail.com>
2020-02-29 20:26:26 +0100

    chap9

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React, { FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
+import PokemonEdit from './pages/pokemon-edit';
 
 
 const App: FunctionComponent = () => {
   
   return (
-  
-    <PokemonList />
+    <Router>
+      <div>
+        {/* la barre de navigation commune à toutes les pages */}
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">Pokédex</Link>
+          </div>
+        </nav>
+        {/* Le systeme de gestion des routes de notre application */}
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemon/edit/:id" component={PokemonEdit} />
+          <Route path="/pokemon/:id" component={PokemonsDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
   
