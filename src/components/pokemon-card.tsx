318d05b0cbe943001a32dc2aac92839ae8459dc4
cyrillecf <cyril.fourcin@gmail.com>
2020-02-29 02:01:03 +0100

    chapitre 6

---
+++ src/components/pokemon-card.tsx
@@ -0,0 +1,39 @@
+import React, {FunctionComponent,useState} from 'react';
+import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+
+type Props = {
+  pokemon: Pokemon,
+  borderColor?: string
+};
+
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
+
+  const [color, setColor] = useState<string>();
+
+  const showBorder = () => {
+    setColor(borderColor);
+  }
+  
+  const hiddenBorder = () => {
+    setColor('#f5f5f5'); // on remet la bordure en gris
+  }
+  
+  return (
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hiddenBorder}>
+      <div className="card horizontal" style={{ borderColor: color }}>
+        <div className="card-image"> 
+          <img src={pokemon.picture} alt={pokemon.name}/>
+        </div>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p>{pokemon.name}</p>
+            <p><small>{pokemon.created.toString()}</small></p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
+}
+
+export default PokemonCard;
